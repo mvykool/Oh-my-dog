ee7ed2aba89666439c25241e3d8fd0bfdd15524a
maicol <maykolandres4@gmail.com>
2023-01-24 04:24:30 -0500

    added menu

--- oh-my-dog/src/components/MenuModal.tsx
+++ oh-my-dog/src/components/MenuModal.tsx
@@ -11,7 +11,7 @@ const MenuModal = ({setMenu}: Props) => {
 
   return (
     <>
-    <div className='fixed w-full bg-black z-[20] top-0 left-0 h-screen ' onClick={() => setMenu(false)} /> 
+    <div className='fixed w-full bg-wrapper z-[20] top-0 left-0 h-screen ' onClick={() => setMenu(false)} /> 
      
         <div
         className=' bg-red-500 scrollLock right-0 z-50 h-[100vh] top-0 w-60 fixed'>
