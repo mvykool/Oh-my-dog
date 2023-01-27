cdd271da851c8017b6992bfff28920267b3a3c1c
maicol <maykolandres4@gmail.com>
2023-01-27 05:50:05 -0500

    added netlify

--- oh-my-dog/astro.config.mjs
+++ oh-my-dog/astro.config.mjs
@@ -7,6 +7,11 @@ import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
 // https://astro.build/config
+import netlify from "@astrojs/netlify/functions";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
+  integrations: [tailwind(), react()],
+  output: "server",
+  adapter: netlify()
 });
