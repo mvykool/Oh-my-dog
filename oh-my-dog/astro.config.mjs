c6831be800f842bc0af2301f9b74f08c562e14ff
maicol <maykolandres4@gmail.com>
2023-01-28 00:02:26 -0500

    set netlify

--- oh-my-dog/astro.config.mjs
+++ oh-my-dog/astro.config.mjs
@@ -6,13 +6,15 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 import react from "@astrojs/react";
 
-
 // https://astro.build/config
 import vercel from "@astrojs/vercel/serverless";
+
+// https://astro.build/config
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
   output: "server",
-  adapter: vercel()
+  adapter: netlify()
 });
